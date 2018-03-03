f4396a86328e16a71d0f0afa2ede5912d99dc6ca
Paul Hanna <paulhanna1933@gmail.com>
2018-03-02 16:25:29 -0800

    update

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
     return (
       <div className="demo-big-content">
     <Layout>
-        <Header title="Title" scroll>
+        <Header className="header-color" title="Title" scroll>
             <Navigation>
                 <Link to="/resume">Resume</Link>
                 <Link to="/aboutme">About Me</Link>
